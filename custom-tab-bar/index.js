c6e90b2500fa19e30061bcb6b2e6daa05a3a5a40
Peter Qiu <273743552@qq.com>
2022-05-27 23:44:25 +0800

    init

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,18 @@
+
+Component({
+  data: {
+    list: [{
+      icon: 'home',
+      value: 'home',
+      label: '首页',
+    },{
+      icon: 'control-platform',
+      value: 'explore',
+      label: '发现',
+    }, {
+      icon: 'user',
+      value: 'user',
+      label: '我的'
+    }]
+  },
+})
