c2ff7b03be6821f9e44943e2da19445e40fb9889
Peter Qiu <273743552@qq.com>
2022-05-29 21:00:54 +0800

    feat: add update manager

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 // app.js
 App({
   onLaunch() {
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // console.log(res.hasUpdate)
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            updateManager.applyUpdate()
+          }
+        }
+      })
     })
   },
   globalData: {
